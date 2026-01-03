ce0f55c168994d058f657beec0b1be19b2970024
Rahul Hegde <rgh9883@rit.edu>
2026-01-03 12:15:55 -0500

    intial setup

---
+++ next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+};
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="relative h-screen w-screen bg-[#008080] overflow-hidden">
+      {/* Taskbar and Windows will be called here */}
+    </main>
+  );
+}
